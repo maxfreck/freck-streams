efd21f80cf1ddd0b63ddf2d1f32f493643a026af
maxfreck <maxim@freck.pp.ru>
2018-03-12 10:25:47 +0400

    Error messages fix

--- source/freck/streams/exception.d
+++ source/freck/streams/exception.d
@@ -7,17 +7,19 @@
  */
 module freck.streams.exception;
 
-string boundsError = "Attempt reading outside of the stream";
-string appendNonreadableStream = "Each stream of AppendStream must be readable";
-string cantOpenFile = "Cant't open file ";
-string cantGetFilestat = "Cant't get filestat ";
-string invalidMode = "Invalid mode specified during file open";
-string nonSeekable = "This stream is not seekable";
-string onlySetOrigin = "This stream can only seek with Seek.set";
-string writeToNonwritable = "This stream is non-writable";
-string readFromNoreadable = "This stream is non-readable";
+package static immutable boundsError = "Attempt reading outside of the stream";
+package static immutable appendNonreadableStream = "Each stream of AppendStream must be readable";
+package static immutable cantOpenFile = "Cant't open file ";
+package static immutable cantGetFilestat = "Cant't get filestat ";
+package static immutable invalidMode = "Invalid mode specified during file open";
+package static immutable nonSeekable = "This stream is not seekable";
+package static immutable onlySetOrigin = "This stream can only seek with Seek.set";
+package static immutable writeToNonwritable = "This stream is non-writable";
+package static immutable readFromNoreadable = "This stream is non-readable";
 
+///Package default exception
 class StreamsException : Exception {
+	///constructor
 	this(string s, string fn = __FILE__, size_t ln = __LINE__) @safe pure nothrow
 	{
 		super(s, fn, ln);
